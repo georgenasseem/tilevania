69ae3718435fd271b33d048ec24bb032edcc4c1d
George Nasseem <Georgenasseem@gmail.com>
2022-05-03 13:19:15 +0400

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathkick = new Vector2 (10f, 10f);
+    
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    Rigidbody2D testValue;
+    CapsuleCollider2D myCapsuleCollider;
+
+    BoxCollider2D myFeetCollider;
+    float gravityScaleAtStart;
+    bool isAlive = true;
+    
+    void Start()
+    {
+        testValue = GetComponent<Rigidbody2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
+    }
+
+    void Update()
+    {
+        if(!isAlive){return;}
+        Run();
+        FlipSprite();
+        ClimbLadder();
+        Die();
+    }
+
+    void OnMove(InputValue value)
+    {
+        if(!isAlive){return;}
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if(!isAlive){return;}
+
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2 (0f, jumpSpeed);
+        }
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+
+    void ClimbLadder()
+    {
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
+        { 
+            myRigidbody.gravityScale = gravityScaleAtStart;
+            myAnimator.SetBool("isClimbing", false);
+            return;
+        }
+        
+        myRigidbody.gravityScale = 0f;
+        Vector2 climbVelocity = new Vector2 (myRigidbody.velocity.x, moveInput.y * climbSpeed);
+        myRigidbody.velocity = climbVelocity;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
+    }
+
+    void Die()
+    {
+        if(myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("enemies", "hazards")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            myRigidbody.velocity = deathkick;
+            FindObjectOfType<GameSession>().ResetLevel();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Tilevania
+
+#### Tilevania is a game made using unity. The goal is to reach the exit. There are 3 levels, once they are all completed you will be welcomed by the most beautiful end screen. Instructions to play Tilevania, controls and contribution details are listed down below.
+
+## Controls
+Controls are pretty simple. Right and left arrows to move. Up and down buttons to move on ladders, and a spacebar to jump.
+
+## Playing Tilevania
+Tilevania is hosted on a [website](https://tilevania.netlify.app) on netlify.
+
+## Contacts
+Feel free to add any changes to the project. 
+You can contact me with my gmail: georgenasseem@gmail.com.
